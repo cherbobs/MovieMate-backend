2acf975d9cdb4ae1ac6b93d79484710b272c2dc7
cherbobs <eva.cherbonnier974@gmail.com>
2025-06-12 09:06:15 +0200

    link back and front

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 // src/index.ts
 import express, { Application, Request, Response } from "express";
 import { supabase } from "./supabaseClient";
+import cors from "cors";
 
 const app: Application = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 // @ts-ignore
@@ -10,8 +14,15 @@ app.get("/", async (_req: Request, res: Response) => {
   const { data, error } = await supabase.from("films").select("*");
   if (error) return res.status(500).json({ error: error.message });
   res.json(data);
+  console.log(data)
 });
 
+// @ts-ignore
+app.get("/users", async (_req: Request, res: Response) => {
+  const { data, error } = await supabase.from("users").select("*");
+  if (error) return res.status(500).json({ error: error.message });
+  res.json(data);
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
